82ddfcbd0c676b10999ffe00b9211e0c4067bca0
0Benedict0 <vladkorinenko123@gmail.com>
2025-07-01 03:39:42 +0300

    fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import { configureStore } from '@reduxjs/toolkit';
+import todoReducer from './todoSlice';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+export const store = configureStore({
+  reducer: {
+    todos: todoReducer,
+  },
+});
